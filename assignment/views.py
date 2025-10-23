c93370e2da89352bf6369945c4815b42f16c9adf
aabccd021 <aabccd021@gmail.com>
2025-10-23 14:33:24 +0700

    Refactor error handling and cursor creation in recordsjson function

--- assignment/views.py
+++ assignment/views.py
@@ -11,20 +11,22 @@ def recordsjson(request):
         user_id = request.data["user_id"]
         word_count = request.data["word_count"]
         timestamp = request.data.get("timestamp", None)
-        print(f"Timestamp: {timestamp}")
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    """
-                    INSERT INTO learning_log (request_id, user_id, word_count, timestamp)
-                    VALUES (%s, %s, %s, COALESCE(%s, CURRENT_TIMESTAMP))
-                    """,
-                    [request_id, user_id, word_count, timestamp],
-                )
-            return Response(None, status=status.HTTP_201_CREATED)
-        except IntegrityError as e:
-            print(e)
-            return Response(None, status=status.HTTP_409_CONFLICT)
+    except KeyError as e:
+        return Response(None, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                INSERT INTO learning_log (request_id, user_id, word_count, timestamp)
+                VALUES (%s, %s, %s, COALESCE(%s, CURRENT_TIMESTAMP))
+                """,
+                [request_id, user_id, word_count, timestamp],
+            )
+        return Response(None, status=status.HTTP_201_CREATED)
+    except IntegrityError as e:
+        print(e)
+        return Response(None, status=status.HTTP_409_CONFLICT)
     except Exception as e:
         print(e)
         return Response(None, status=status.HTTP_400_BAD_REQUEST)
