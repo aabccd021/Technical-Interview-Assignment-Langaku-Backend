59e4187a4ccccf8a860a3a3e3a9521955e212a24
aabccd021 <aabccd021@gmail.com>
2025-10-23 11:14:18 +0700

    Remove unused and deleted files, middleware for mock login removed.

--- assignment/settings.py
+++ assignment/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "assignment.middleware.MockLoginUserMiddleware",
 ]
 
 ROOT_URLCONF = "assignment.urls"
